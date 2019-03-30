32396cb0dfe5456319100f4271435c59c79ec3c1
danilomo <danilomendoncaoliveira@gmail.com>
2019-03-30 13:39:21 +0100

    Initial commit

---
+++ init.el
@@ -0,0 +1,60 @@
+;; DO NOT MODIFY THE LINES BELLOW
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl
+    (warn "\Your version of Emacs does not support SSL connections,
+           which is unsafe because it allows man-in-the-middle attacks.
+           There are two things you can do about this warning:
+           1. Install an Emacs version that does support SSL and be safe.
+           2. Remove this warning from your init file so you won't see it again."))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (darcula-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; DO NOT MODIFY THE LINES ABOVE
+
+;; Darcula theme needs it
+(eval-when-compile
+  (require 'use-package))
+
+;; The theme
+(use-package darcula-theme
+  :ensure t
+  :config
+  (set-frame-font "Inconsolata-14"))
+
+;; Recent files
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(setq recentf-max-saved-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+;; Tabs instead of spaces
+(setq-default indent-tabs-mode nil)
+
+;; Disable menu/scroll/tool bars 
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1) 
+
+;; Disable automatic backup/autosave
+(setq make-backup-files nil) ; stop creating backup~ files
+(setq auto-save-default nil) ; stop creating #autosave# files

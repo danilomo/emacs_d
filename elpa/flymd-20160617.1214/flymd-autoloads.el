9ab0c7feff71ecabc6fc1e2ff6b3adf81b774aad
Danilo Oliveira <danilomendoncaoliveira@gmail.com>
2019-06-25 09:30:14 +0200

    Installing markdown support

---
+++ elpa/flymd-20160617.1214/flymd-autoloads.el
@@ -0,0 +1,25 @@
+;;; flymd-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "flymd" "flymd.el" (23726 60417 87498 46000))
+;;; Generated autoloads from flymd.el
+
+(autoload 'flymd-flyit "flymd" "\
+Enable realtime markdown preview on the current buffer.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("flymd-pkg.el") (23726 60417 79498 159000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flymd-autoloads.el ends here
--- init.el
+++ init.el
@@ -21,7 +21,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (use-package darcula-theme))))
+ '(package-selected-packages
+   (quote
+    (markdown-preview-mode markdown-preview-eww markdown-mode+ markdown-mode flymd use-package darcula-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -60,3 +62,7 @@
 (setq auto-save-default nil) ; stop creating #autosave# files
 
 ;;
+
+(setq-default c-basic-offset 4
+              tab-width 4
+              indent-tabs-mode t)
